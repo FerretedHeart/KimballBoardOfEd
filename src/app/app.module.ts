166329418571f370034aca16c2a95c313689b3db
Jeanine Kimball <jeanine.marie@me.com>
2021-09-24 15:04:43 -0400

    Added news component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { CampaignComponent } from './campaign/campaign.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CampaignComponent,
     WelcomeComponent,
-    FooterComponent
+    FooterComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: '', component: WelcomeComponent},
       {path: 'campaign', component: CampaignComponent},
+      {path: 'news', component: NewsComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
     ]),
